8777452ad9b283759b5eae7929e83c3c968e18cb
mahowald <matthew.mahowald@gmail.com>
2016-10-26 22:05:23 -0500

    Added camera zooming.

---
+++ Assets/Resources/scripts/utility/Zoomable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoomable : MonoBehaviour {
+
+    public Transform zoomIn;
+    public Transform zoomOut;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateZoom();
+        transform.position = Vector3.Lerp(zoomOut.position, zoomIn.position, cameraDistance);
+        transform.rotation = Quaternion.Lerp(zoomOut.rotation, zoomIn.rotation, cameraDistance);
+	}
+
+    float scrollVelocity = 0f;
+    float lastScroll = 0f;
+    float cameraDistance = 0f;
+    float cameraSpeed = 30f;
+    void UpdateZoom()
+    {
+        var scroll = Input.GetAxis("LookZoom") * cameraSpeed * Time.deltaTime;
+        scroll = Mathf.SmoothDamp(lastScroll, scroll, ref scrollVelocity, 0.1f);
+
+        if (scroll > 0 && cameraDistance <= 1f )
+        {
+            cameraDistance += scroll;
+        }
+        if (scroll < 0 && cameraDistance >= 0f )
+        {
+            cameraDistance += scroll;
+        }
+
+        lastScroll = scroll;
+    }
+}
