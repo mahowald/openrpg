4fcbb2b1cd70f2ff6312da7341631d03320a7296
mahowald <matthew.mahowald@gmail.com>
2016-11-11 19:47:06 -0600

    Projector follows selected actor.

---
+++ Assets/Resources/scripts/visual_effects/ProjectorBase.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectorBase : MonoBehaviour {
+
+    public GameObject projectorTop;
+    public Transform follow;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        this.transform.position = follow.position;
+        LookAtMouse();
+    }
+
+    bool on = false;
+    void SetFollowActor(ActorSystem.Actor a)
+    {
+        follow = a.transform;
+    }
+    public void SetProjector()
+    {
+        on = !on;
+        projectorTop.SetActive(on);
+    }
+
+    public void LookAtMouse()
+    {
+        GetMousePosition();
+        transform.rotation = Quaternion.LookRotation(mousePosition - transform.position, Vector3.up);
+    }
+
+    Vector3 mousePosition = Vector3.zero;
+
+    void GetMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            mousePosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+        }
+    }
+
+    // Subscribe/Unsubscribe to our events
+    public void OnEnable()
+    {
+        EventManager.StartListening("ButtonPressed", SetProjector);
+        EventManager.StartListening<ActorSystem.Actor>("ActorClicked", SetFollowActor);
+    }
+    public void OnDisable()
+    {
+        EventManager.StopListening("ButtonPressed", SetProjector);
+        EventManager.StopListening<ActorSystem.Actor>("ActorClicked", SetFollowActor);
+    }
+
+
+}
