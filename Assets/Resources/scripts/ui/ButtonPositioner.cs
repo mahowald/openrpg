193ccf4f500bd172f47db61892763b21f76e6b44
mahowald <matthew.mahowald@gmail.com>
2017-06-03 22:51:22 -0500

    Contextual UI fragments

---
+++ Assets/Resources/scripts/ui/ButtonPositioner.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+    // nicely layout buttons on a canvas
+    public static class ButtonPositioner
+    {
+        public static void layout(List<RectTransform> items)
+        {
+            
+            float maxWidth = -1f;
+            float maxHeight = -1f;
+
+            foreach(RectTransform item in items)
+            {
+                if(item.rect.width > maxWidth)
+                    maxWidth = item.rect.width/2f;
+                if (item.rect.width > maxHeight)
+                    maxHeight = item.rect.height/2f;
+            }
+            
+            // had to look up the radius of a regular polygon...
+            float radius = maxWidth / (2*Mathf.Sin(Mathf.PI / items.Count));
+            float ratio = maxHeight / maxWidth;
+            
+            List<Vector2> arcPos = arcPositions(items.Count);
+            // scale by the side length, and then offset each button by their center
+            arcPos = transform(arcPos, new Vector4(2f*radius, 0f, 0f, 2f*radius), new Vector2(-1*maxWidth, -1*maxHeight));
+
+            foreach(Vector2 pos in arcPos)
+            {
+                Debug.Log(pos);
+            }
+
+            for(int i = 0; i < items.Count; i++)
+            {
+                RectTransform item = items[i];
+                item.anchoredPosition = arcPos[i];
+            }
+        }
+
+        /// <summary>
+        /// Create a list of positions in a circle.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static List<Vector2> arcPositions(int n)
+        {
+            if (n <= 0)
+            {
+                throw new System.Exception("Invalid integer.");
+            }
+            if(n == 1)
+            {
+                List<Vector2> pos = new List<Vector2>(n);
+                pos[0] = new Vector2(0f, 0f);
+                return pos;
+            }
+            float baseangle = 2 * Mathf.PI / n;
+
+            List<Vector2> positions = new List<Vector2>(new Vector2[n]);
+            for(int i = 0; i < n; i++)
+            {
+                float angle = i * baseangle;
+                float sine = Mathf.Sin(angle);
+                float cosine = Mathf.Cos(angle);
+                positions[i] = new Vector2(cosine, sine);
+            }
+
+            return positions;
+        }
+
+        /// <summary>
+        /// Apply a linear transformation to every item in the list.
+        /// </summary>
+        /// <param name="positions"></param>
+        /// <param name="matrix">A 2x2 matrix of transformations. Format is (x, y, z, w)</param>
+        /// <returns></returns>
+        private static List<Vector2> transform(List<Vector2> positions,  Vector4 matrix, Vector2 offset)
+        {
+            List<Vector2> newPositions = new List<Vector2>(new Vector2[positions.Count]);
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Vector2 v = positions[i];
+                float newx = v.x * matrix.x + v.y * matrix.y + offset.x;
+                float newy = v.x * matrix.z + v.y * matrix.w + offset.y;
+                newPositions[i] = new Vector2(newx, newy);
+            }
+            return newPositions;
+        }
+
+        private static List<Vector2> transform(List<Vector2> positions, Vector4 matrix)
+        {
+            return transform(positions, matrix, new Vector2(0f, 0f));
+        }
+
+        private static List<Vector2> transform(List<Vector2> positions, Vector2 offset)
+        {
+            return transform(positions, new Vector4(1f, 0f, 1f, 0f), offset);
+        }
+    }
+
+}
--- Assets/Resources/scripts/ui/UIBase.cs
+++ Assets/Resources/scripts/ui/UIBase.cs
@@ -2,101 +2,137 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class UIBase : MonoBehaviour {
-
-    private Vector3 pointContext;
-    private ActorSystem.Actor actorContext;
-
-    public Camera mycamera;
-    public RectTransform uiPanel;
-
-	// Use this for initialization
-	void Start () {
-        uiPanel.gameObject.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    void Relocate(Vector3 dest, Vector3 offset)
+namespace UserInterface
+{
+    public class UIBase : MonoBehaviour
     {
-        uiPanel.anchoredPosition = mycamera.WorldToScreenPoint(dest + offset);
-    }
 
-    void ActorSelected(ActorSystem.Actor actor)
-    {
-        actorContext = actor;
-        Vector3 offset = new Vector3(0f, 2f, 0f);
-        uiPanel.gameObject.SetActive(true);
-        Relocate(actor.Position, offset);
-    }
+        private Vector3 pointContext;
+        private ActorSystem.Actor actorContext;
 
-    void PointSelected(Vector3 point)
-    {
-        pointContext = point;
-        actorContext = null;
-        Vector3 offset = Vector3.zero;
-        uiPanel.gameObject.SetActive(true);
-        Relocate(point, offset);
-    }
+        public Camera mycamera;
+        public RectTransform uiPanel;
 
-    // Subscribe/Unsubscribe to our events
-    public void OnEnable()
-    {
-        EventManager.StartListening<ActorSystem.Actor>("ContextClick", ActorSelected);
-        EventManager.StartListening<Vector3>("ContextClick", PointSelected);
-    }
-    public void OnDisable()
-    {
-        EventManager.StopListening<ActorSystem.Actor>("ContextClick", ActorSelected);
-        EventManager.StopListening<Vector3>("ContextClick", PointSelected);
-    }
+        public RectTransform moveButton;
+        public RectTransform attackButton;
+        public RectTransform swapButton;
+        public RectTransform abilityButton;
 
-    // buttons
-    public void Swap()
-    {
-        if(actorContext != null)
+        // Use this for initialization
+        void Start()
         {
-            EventManager.TriggerEvent("ActorClicked", actorContext);
-            actorContext = null;
             uiPanel.gameObject.SetActive(false);
         }
-    }
 
-    public void Move()
-    {
-        Geometry.Locatable target = new Geometry.Locatable();
-        if (actorContext == null)
+        // Update is called once per frame
+        void Update()
         {
-            target.Position = pointContext;
+
         }
-        else
+
+        void Relocate(Vector3 dest, Vector3 offset)
         {
-            target.Position = actorContext.Position;
+            uiPanel.anchoredPosition = mycamera.WorldToScreenPoint(dest + offset);
         }
-        target.Direction = Vector3.zero;
-        ActorSystem.IAction action = new ActorSystem.LocatableEmptyAction(GameController.PlayerActor, target);
-        EventManager.TriggerEvent("DoAction", GameController.PlayerActor, action);
-        uiPanel.gameObject.SetActive(false);
-    }
 
-    public void Attack()
-    {
-        if (actorContext != null)
+        void ActorSelected(ActorSystem.Actor actor)
         {
-            // TODO: Think about how this should really be done.
-            ActorSystem.SingleTargetDamageActionPrototype actionPrototype = new ActorSystem.SingleTargetDamageActionPrototype();
-            actionPrototype.Cooldown = new Expression("0");
-            actionPrototype.Cost = new Dictionary<string, Expression>();
-            actionPrototype.Damage = new Dictionary<string, Expression>(); //  { { "health", new Expression("5") } };
-            actionPrototype.Range = new Expression("1");
-            actionPrototype.Animation = ActorSystem.ActionAnimation.BasicAttack;
-            ActorSystem.IAction action = actionPrototype.Instantiate(GameController.PlayerActor, actorContext);
+            actorContext = actor;
+            Vector3 offset = new Vector3(0f, 2f, 0f);
+            uiPanel.gameObject.SetActive(true);
+            Relocate(actor.Position, offset);
 
+            moveButton.gameObject.SetActive(false);
+            abilityButton.gameObject.SetActive(false);
+            attackButton.gameObject.SetActive(true);
+            swapButton.gameObject.SetActive(true);
+
+            List<RectTransform> buttons = new List<RectTransform>
+            {
+                attackButton,
+                swapButton
+            };
+
+            ButtonPositioner.layout(buttons);
+        }
+
+        void PointSelected(Vector3 point)
+        {
+            pointContext = point;
+            actorContext = null;
+            Vector3 offset = Vector3.zero;
+            uiPanel.gameObject.SetActive(true);
+            Relocate(point, offset);
+
+            moveButton.gameObject.SetActive(true);
+            abilityButton.gameObject.SetActive(true);
+            attackButton.gameObject.SetActive(false);
+            swapButton.gameObject.SetActive(false);
+
+            List<RectTransform> buttons = new List<RectTransform>
+            {
+                moveButton,
+                abilityButton
+            };
+            ButtonPositioner.layout(buttons);
+        }
+
+        // Subscribe/Unsubscribe to our events
+        public void OnEnable()
+        {
+            EventManager.StartListening<ActorSystem.Actor>("ContextClick", ActorSelected);
+            EventManager.StartListening<Vector3>("ContextClick", PointSelected);
+        }
+        public void OnDisable()
+        {
+            EventManager.StopListening<ActorSystem.Actor>("ContextClick", ActorSelected);
+            EventManager.StopListening<Vector3>("ContextClick", PointSelected);
+        }
+
+        // buttons
+        public void Swap()
+        {
+            if (actorContext != null)
+            {
+                EventManager.TriggerEvent("ActorClicked", actorContext);
+                actorContext = null;
+                uiPanel.gameObject.SetActive(false);
+            }
+        }
+
+        public void Move()
+        {
+            Geometry.Locatable target = new Geometry.Locatable();
+            if (actorContext == null)
+            {
+                target.Position = pointContext;
+            }
+            else
+            {
+                target.Position = actorContext.Position;
+            }
+            target.Direction = Vector3.zero;
+            ActorSystem.IAction action = new ActorSystem.LocatableEmptyAction(GameController.PlayerActor, target);
             EventManager.TriggerEvent("DoAction", GameController.PlayerActor, action);
             uiPanel.gameObject.SetActive(false);
+        }
+
+        public void Attack()
+        {
+            if (actorContext != null)
+            {
+                // TODO: Think about how this should really be done.
+                ActorSystem.SingleTargetDamageActionPrototype actionPrototype = new ActorSystem.SingleTargetDamageActionPrototype();
+                actionPrototype.Cooldown = new Expression("0");
+                actionPrototype.Cost = new Dictionary<string, Expression>();
+                actionPrototype.Damage = new Dictionary<string, Expression>(); //  { { "health", new Expression("5") } };
+                actionPrototype.Range = new Expression("1");
+                actionPrototype.Animation = ActorSystem.ActionAnimation.BasicAttack;
+                ActorSystem.IAction action = actionPrototype.Instantiate(GameController.PlayerActor, actorContext);
+
+                EventManager.TriggerEvent("DoAction", GameController.PlayerActor, action);
+                uiPanel.gameObject.SetActive(false);
+            }
         }
     }
 }
